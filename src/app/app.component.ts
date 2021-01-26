f207ac6ccdd1f15cf3ab50301c9fcd5270818768
itsmvinay <itsmvinay@gmail.com>
2021-01-26 22:28:51 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'LocationData';
+
+
+  //set area details to false selected intially
+  mySelection: boolean = false;
+  somevalue: string = "";
+  constructor() {
+    console.log(this.somevalue);
+  }
+  //array of objects : containing data related to the location
+  locationDetails = [
+    { name: "srinagar", population: { total: 5900, men: 4000, women: 1900 }, literacy: "44%" },
+    { name: "Hanumantha Nagar", population: { total: 8400, men: 4400, women: 4400 }, literacy: "67%" },
+    { name: "N R colony", population: { total: 5000, men: 5000, women: 2000 }, literacy: "64%" },
+    { name: "Gandhi Bazar", population: { total: 9300, men: 4300, women: 5000 }, literacy: "94%" },
+    { name: "D V G Road", population: { total: 4600, men: 2000, women: 2600 }, literacy: "94%" }
+  ];
+
+  //set an area initially as default
+  selectedLocation = this.locationDetails[1];
+
+  //event to show details of area on selection of drop down
+  onChangeObj(newObj: any) {
+    console.log(newObj);
+    this.selectedLocation = newObj;
+    this.mySelection = true;
+  }
 }
+
+
+
